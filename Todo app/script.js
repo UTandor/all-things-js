8240d51c91eac330ce8aa1f8fc773bad8a427f22
UTandor <demonsouls34865@gmail.com>
2024-02-18 13:08:28 +0500

    AJAJAJAJJAJAJAJAJAJAJ

--- Todo app/script.js
+++ Todo app/script.js
@@ -1,49 +1,126 @@
 let todoList = [];
+
 function handleTodo() {
-  const todo = document.getElementById("todo-input").value;
   try {
-    if (todo !== "") {
-      todoList.push({ id: todoList.length + 1, todo: todo, completed: false });
-      const li = document.createElement("li");
-      li.innerHTML = todo;
-      li.id = `todo-${todoList.length}`;
-      li.classList = "todo";
-      document.getElementById("todos").appendChild(li);
+    const todoInput = document.getElementById("todo-input");
+    const submitButton = document.getElementById("submit");
 
-      const deleteButton = document.createElement("button");
-      deleteButton.innerHTML = "D";
-      deleteButton.classList = "delete";
-
-      const editButton = document.createElement("button");
-      editButton.innerHTML = "E";
-      editButton.classList = "edit";
-
-      deleteButton.addEventListener("click", function () {
-        deleteItem(
-          todoList.findIndex((x) => x.id === Number(li.id.split("-")[1]))
-        );
-      });
-
-      editButton.addEventListener("click", function () {
-        editItem(
-          todoList.findIndex((x) => x.id === Number(li.id.split("-")[1]))
-        );
-      });
-
-      li.appendChild(editButton);
-      li.appendChild(deleteButton);
-
-      document.getElementById("todo-input").value = "";
-    } else {
+    if (todoInput.value.trim() === "") {
       return false;
     }
+
+    const todo = {
+      id: todoList.length,
+      text: todoInput.value.trim(),
+      completed: false,
+    };
+
+    if (submitButton.innerHTML === "Submit") {
+      addTodoItem(todo);
+    } else if (submitButton.innerHTML === "Edit") {
+      editTodoItem(todo);
+    }
+
+    todoInput.value = "";
   } catch (error) {
     console.error(error);
   }
 }
 
-function deleteItem(index) {
-  todoList.splice(index, 1);
-  const li = document.querySelector(`#todo-${index + 1}`);
-  li.remove();
+function addTodoItem(todo) {
+  todoList.push(todo);
+
+  const li = createTodoListItem(todo);
+  document.getElementById("todos").appendChild(li);
+}
+
+function createTodoListItem(todo) {
+  const li = document.createElement("li");
+  li.innerHTML = todo.text;
+  li.id = `todo-${todo.id}`;
+  li.classList = "todo";
+
+  const deleteButton = createButton("D", "delete", () => deleteItem(todo.id));
+  const editButton = createButton("E", "edit", () => editItem(todo.id));
+
+  li.appendChild(editButton);
+  li.appendChild(deleteButton);
+
+  return li;
+}
+
+function createButton(text, className, clickHandler) {
+  const button = document.createElement("button");
+  button.innerHTML = text;
+  button.classList = className;
+  button.addEventListener("click", clickHandler);
+  return button;
+}
+
+function deleteItem(id) {
+  const index = todoList.findIndex((x) => x.id === id);
+
+  if (index !== -1) {
+    todoList.splice(index, 1);
+    const li = document.getElementById(`todo-${id}`);
+    if (li) {
+      li.remove();
+    }
+
+    updateListIds();
+  }
+}
+
+function updateListIds() {
+  todoList.forEach((todo, index) => {
+    const listItem = document.getElementById(`todo-${todo.id}`);
+    if (listItem) {
+      listItem.id = `todo-${index}`;
+    }
+    todo.id = index;
+  });
+}
+
+function editItem(id) {
+  const index = todoList.findIndex((x) => x.id === id);
+
+  if (index !== -1) {
+    const todo = todoList[index];
+    const li = document.getElementById(`todo-${id}`);
+
+    if (li) {
+      const editInput = document.createElement("input");
+      editInput.type = "text";
+      editInput.value = todo.text;
+
+      const saveButton = createButton("Save", "save", () => saveEdit(id, editInput));
+      const cancelButton = createButton("Cancel", "cancel", () => cancelEdit(id));
+
+      li.innerHTML = "";
+      li.appendChild(editInput);
+      li.appendChild(saveButton);
+      li.appendChild(cancelButton);
+    }
+  }
+}
+
+function saveEdit(id, editInput) {
+  const index = todoList.findIndex((x) => x.id === id);
+
+  if (index !== -1) {
+    const newTodoText = editInput.value;
+    todoList[index].text = newTodoText;
+
+    const li = createTodoListItem(todoList[index]);
+    document.getElementById("todos").replaceChild(li, document.getElementById(`todo-${id}`));
+  }
+}
+
+function cancelEdit(id) {
+  const index = todoList.findIndex((x) => x.id === id);
+
+  if (index !== -1) {
+    const li = createTodoListItem(todoList[index]);
+    document.getElementById("todos").replaceChild(li, document.getElementById(`todo-${id}`));
+  }
 }
