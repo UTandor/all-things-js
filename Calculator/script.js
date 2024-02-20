96c86e22c6eb5ec2b08a0c762fa4dae521b64416
UTandor <demonsouls34865@gmail.com>
2024-02-20 12:40:43 +0500

    Dont wanna lose this list. hence commiting

---
+++ Calculator/script.js
@@ -0,0 +1,24 @@
+const buttons = [
+  { id: 'clear', name: 'C' },
+  { id: 'erase', name: 'E' },
+  { id: 'zero', name: '0' },
+  { id: 'one', name: '1' },
+  { id: 'two', name: '2' },
+  { id: 'three', name: '3' },
+  { id: 'four', name: '4' },
+  { id: 'five', name: '5' },
+  { id: 'six', name: '6' },
+  { id: 'seven', name: '7' },
+  { id: 'eight', name: '8' },
+  { id: 'nine', name: '9' },
+  { id: 'add', name: '+' },
+  { id: 'subtract', name: '-' },
+  { id: 'multiply', name: '*' },
+  { id: 'divide', name: '/' },
+  { id: 'equals', name: '=' },
+  { id: 'decimal', name: '.' },
+];
+
+console.log(buttons[0].id);
+console.log(buttons[0].name); 
+
