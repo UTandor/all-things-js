09ed88f14c2745ff178445c5671e30d3a7ca3473
UTandor <demonsouls34865@gmail.com>
2024-02-20 11:37:01 +0500

    Haha pyramid go brrrrr

---
+++ Pyramid/script.js
@@ -0,0 +1,44 @@
+let form = document.getElementById("form");
+let ul = document.getElementById("list");
+
+function leftToRight() {
+  const input = document.getElementById("size").value;
+  console.log(input);
+
+  ul.innerHTML = "";
+
+  for (let i = 0; i < input; i += 1) {
+    const li = document.createElement("li");
+    li.innerHTML = "#".repeat(i + 1);
+    ul.appendChild(li);
+  }
+}
+
+function rightToLeft() {
+  const input = document.getElementById("size").value;
+  console.log(input);
+
+  ul.innerHTML = "";
+
+  for (let i = 0; i < input; i += 1) {
+    const li = document.createElement("li");
+    li.innerHTML = "#".repeat(input - i);
+    ul.appendChild(li);
+  }
+}
+
+function rightAndLeft() {
+  const input = document.getElementById("size").value;
+
+  ul.innerHTML = "";
+  for (let i = 0; i < input; i += 1) {
+    const li = document.createElement("li");
+    li.innerHTML = "#".repeat(i + 1);
+    ul.appendChild(li);
+  }
+  for (let i = 0; i < input; i += 1) {
+    const li = document.createElement("li");
+    li.innerHTML = "#".repeat(input - i);
+    ul.appendChild(li);
+  }
+}
