f9fc07e80a7f718127ffcb9a4aab2f1ba2c7ba17
UTandor <demonsouls34865@gmail.com>
2024-02-22 14:20:22 +0500

    This is all I think that ill do. Will come back when i'm allowed to use tailwindcss

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,43 +1,78 @@
 const buttons = [
-  { id: "clear", name: "AC" },
-  { id: "clear", name: "+/-" },
-  { id: "erase", name: "C" },
-  { id: "add", name: "+" },
-  { id: "one", name: "1" },
-  { id: "two", name: "2" },
-  { id: "three", name: "3" },
-  { id: "subtract", name: "-" },
-  { id: "four", name: "4" },
-  { id: "five", name: "5" },
-  { id: "six", name: "6" },
-  { id: "multiply", name: "*" },
-  { id: "seven", name: "7" },
-  { id: "eight", name: "8" },
-  { id: "nine", name: "9" },
-  { id: "divide", name: "/" },
-  { id: "zero", name: "0" },
-  { id: "equals", name: "=" },
-  { id: "decimal", name: "." },
+  { id: "clear", name: "AC", content: "🆑" },
+  { id: "plus-minus", name: "**", content: "⬛" },
+  { id: "erase", name: "C", content: "🔄" },
+  { id: "add", name: "+", content: "➕" },
+  { id: "one", name: "1", content: "1️⃣" },
+  { id: "two", name: "2", content: "2️⃣" },
+  { id: "three", name: "3", content: "3️⃣" },
+  { id: "subtract", name: "-", content: "➖" },
+  { id: "four", name: "4", content: "4️⃣" },
+  { id: "five", name: "5", content: "5️⃣" },
+  { id: "six", name: "6", content: "6️⃣" },
+  { id: "multiply", name: "*", content: "✖️" },
+  { id: "seven", name: "7", content: "7️⃣" },
+  { id: "eight", name: "8", content: "8️⃣" },
+  { id: "nine", name: "9", content: "9️⃣" },
+  { id: "divide", name: "/", content: "➗" },
+  { id: "zero", name: "0", content: "0️⃣" },
+  { id: "equals", name: "=", content: "➡️" },
+  { id: "decimal", name: ".", content: "🔵" },
 ];
 
 const container = document.getElementById("container");
 let formula = "";
 
-const changeFormula = (name) => {
+const changeFormula = () => {
+  console.log(formula);
   const answer = document.getElementById("answer");
-  answer.innerHTML = name;
+  answer.innerHTML = formula;
+};
+
+const calculateAnswer = () => {
+  try {
+    console.log(formula)
+    formula = eval(formula);
+    console.log(formula)
+    return formula
+  } catch (e) {
+    console.error(e);
+    return "Error"
+  }
 };
 
 for (let i = 0; i < buttons.length; i += 1) {
   const button = document.createElement("button");
-  button.innerHTML = buttons[i].name;
+  button.innerHTML = buttons[i].content;
   button.id = buttons[i].id;
   button.name = buttons[i].name;
   button.classList = "button";
-  button.addEventListener("click", () => changeFormula(button.name));
+
+  switch (button.name) {
+    case "AC":
+      button.addEventListener("click", () => {
+        formula = "";
+        changeFormula(button.name);
+      });
+      break;
+    case "C":
+      button.addEventListener("click", () => {
+        formula = formula.slice(0, -1);
+        changeFormula(button.name);
+      });
+      break;
+    case "=":
+      button.addEventListener("click", () => {
+        formula = calculateAnswer();
+        changeFormula(button.name);
+      });
+      break;
+    default:
+      button.addEventListener("click", () => {
+        formula += button.name;
+        changeFormula(button.name);
+      });
+  }
 
   container.appendChild(button);
 }
-
-console.log(buttons[0].id);
-console.log(buttons[0].name);
