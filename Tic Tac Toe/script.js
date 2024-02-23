3038063fa0c1b9d40ae145db0b18c7134878ee26
UTandor <demonsouls34865@gmail.com>
2024-02-23 14:40:51 +0500

    Working on extra features

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -11,8 +11,11 @@ const player = document.createElement("h3");
 player.id = "turn";
 document.body.appendChild(player);
 
-let winnerElement = document.createElement("p");
+let winnerElement = document.createElement("strong");
 document.body.appendChild(winnerElement);
+
+let winConditionElement = document.createElement("p");
+document.body.appendChild(winConditionElement);
 
 const renderBoard = (persistTurn) => {
   const boxes = document.querySelectorAll(".box");
@@ -49,24 +52,47 @@ const renderBoard = (persistTurn) => {
 
 const checkWinner = () => {
   let winner = "";
+  let winCondition = "";
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length - 2; j++) {
-      if (board[i][j] !== '.' && board[i][j] === board[i][j + 1] && board[i][j] === board[i][j + 2]) {
+      if (
+        board[i][j] !== "." &&
+        board[i][j] === board[i][j + 1] &&
+        board[i][j] === board[i][j + 2]
+      ) {
         winner = board[i][j];
         console.log("Horizontal Winner:", winner);
       }
 
-      if (board[j][i] !== '.' && board[j][i] === board[j + 1][i] && board[j][i] === board[j + 2][i]) {
+      if (
+        board[j][i] !== "." &&
+        board[j][i] === board[j + 1][i] &&
+        board[j][i] === board[j + 2][i]
+      ) {
         winner = board[j][i];
         console.log("Vertical Winner:", winner);
+      }
+
+      if (
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2] &&
+        board[0][0] !== "."
+      ) {
+        winner = board[0][0];
+      }
+      if (
+        board[0][2] === board[1][1] &&
+        board[0][2] === board[2][0] &&
+        board[0][2] !== "."
+      ) {
+        winner = board[0][2];
       }
     }
   }
 
-  if (winner !== "") {
-    console.log("winner found");
-
+  if (winner !== "" && winCondition !== "") {
+    winConditionElement.innerHTML = "Won via: " + winCondition;
     winnerElement.innerHTML = "Winner: " + winner;
   }
 };
