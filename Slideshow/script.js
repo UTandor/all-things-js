c8131a6ec7ee4d2c88e4118f5a7cb24c2d99c33b
UTandor <demonsouls34865@gmail.com>
2024-02-23 22:22:49 +0500

    Added event listners. Last commit of the day

--- Slideshow/script.js
+++ Slideshow/script.js
@@ -1,20 +1,25 @@
 const carouselElement = document.getElementById("carousel");
 const nav = document.getElementById("nav");
 
+let currPage = 0;
+const pages = [];
+
 const rightButton = document.createElement("button");
 rightButton.id = "right";
 rightButton.innerHTML = "Right";
+rightButton.addEventListener("click", () => {
+  if (currPage !== pages.length + 1) currPage += 1;
+});
 
 const leftButton = document.createElement("button");
 leftButton.id = "left";
 leftButton.innerHTML = "Left";
+leftButton.addEventListener("click", () => {
+  if (currPage < pages.length) currPage -= 1;
+});
 
 nav.appendChild(rightButton);
 nav.appendChild(leftButton);
-
-const pages = [];
-
-
 
 const page1 = document.createElement("div");
 page1.innerHTML = "This is page 1";
@@ -31,4 +36,5 @@ pages.push(page3);
 pages.forEach((page) => {
   carouselElement.appendChild(page);
 });
+
 
